2b2784229723cd62a8c6188953198804b97b5acd
Sura <375262986@qq.com>
2020-04-04 23:26:35 +0800

    feat: add example
    add test example

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'post'
+  | 'POST'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
