a04207441db174ca119aff88ef8b3b910bbc8fa1
Sura <375262986@qq.com>
2020-04-07 01:03:08 +0800

    feat: add header transform

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeader } from './helpers/headers'
 import xhr from './xhr'
 
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -29,6 +31,11 @@ function transformHeaders(config: AxiosRequestConfig): any {
   // headers 要添加默认值
   const { headers = {}, data } = config
   return processHeader(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
