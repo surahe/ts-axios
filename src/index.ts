495c8704565f1c1e10aedb107e2e4a968ef049c5
Sura <375262986@qq.com>
2020-04-06 20:14:51 +0800

    feat: add data & header config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeader } from './helpers/headers'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
@@ -9,11 +11,24 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  // 先处理headers，否则会被转换成字符串
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  // headers 要添加默认值
+  const { headers = {}, data } = config
+  return processHeader(headers, data)
 }
 
 export default axios
