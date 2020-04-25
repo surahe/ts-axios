49203ec047a77db7bd3bb214cf18974ac7edfba1
Sura <375262986@qq.com>
2020-04-26 01:35:11 +0800

    feat: add error handle

---
+++ src/helpers/error.ts
@@ -0,0 +1,40 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    this.isAxiosError = true
+
+    // 修复 ts 继承部分Error, Array, Map 的问题
+    // 参考：https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+): AxiosError {
+  const error = new AxiosError(message, config, code, request, response)
+
+  return error
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeader } from './helpers/headers'
-import xhr from './xhr'
+import axios from './axios'
 
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  // 先处理headers，否则会被转换成字符串
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  // headers 要添加默认值
-  const { headers = {}, data } = config
-  return processHeader(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+export * from './types'
 
 export default axios
